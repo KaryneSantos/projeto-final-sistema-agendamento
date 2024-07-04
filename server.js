616a32584ddd02375f62995d9808429c1b72e857
Karyne Santos <karynesantos0901@gmail.com>
2024-07-03 21:33:37 -0300

    novas atualizações:
    1. linkando todos os links para suas páginas
    2. fazendo o layout da página inicial

--- server.js
+++ server.js
@@ -11,6 +11,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const paginaInicial = require('./routers/index');
 app.use('/', paginaInicial);
 
+// Página Inicial de Agendamento
+const agendamentoPagina = require('./routers/agendamento-pagina');
+app.use('/agendamento-pagina-inicial', agendamentoPagina);
+
+// Página de Contato (Inicial)
+const contatoPagina = require('./routers/contato');
+app.use('/contato', contatoPagina);
+
+// Página de Cadastro (Médico)
+const cadastroMedico = require('./routers/cadastro-medico');
+app.use('/cadastro-medico', cadastroMedico);
+
+// Página de Cadastro (Paciente)
+const cadastroPaciente = require('./routers/cadastro-paciente');
+app.use('/cadastro-paciente', cadastroPaciente);
+
+//Página de Login
+const paginaLogin = require('./routers/paginaLogin');
+app.use('/login', paginaLogin);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 console.log(`O servidor está rodando na porta ${PORT}`);
